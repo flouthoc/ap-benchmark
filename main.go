1e6f821917ca62519d02df54f2483b972428f75e
flouthoc <flouthoc.git@gmail.com>
2024-04-17 23:23:45 -0700

    Clean and put everything in gofile

--- main.go
+++ main.go
@@ -1,76 +1,104 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"github.com/mattn/go-mastodon"
+	"io/ioutil"
+	"log"
+	"bufio"
+	"math/rand"
+	"net/http"
 	"os"
-	"strconv"
+	"os/exec"
+	"strings"
 	"sync"
 	"time"
+
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/plotutil"
+	"gonum.org/v1/plot/vg"
 )
 
-func createToots(client *mastodon.Client, deleteToot bool) {
-	start := time.Now()
-	status, err := client.PostStatus(context.Background(), &mastodon.Toot{
-		Status: "Setting sample status",
-	})
-	if err != nil {
-		fmt.Printf("Error %w while setting the status/toot\n", err)
-		panic(err)
-	}
-	t := time.Now()
-	fmt.Printf("Duration: %+v to post a status\n", t.Sub(start))
+const METRICS_PATH = "/tmp/tweet_fan_out_metrics"
 
-	fmt.Println("Posted status id is: " + status.ID)
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
 
-	if deleteToot {
-		start = time.Now()
-		// delete the newly created status
-		client.DeleteStatus(context.Background(), status.ID)
-		t = time.Now()
-		fmt.Printf("Duration: %+v to delete a status \n", t.Sub(start))
-	}
+type AccountCreateRequest struct {
+	// Text that will be reviewed by moderators if registrations require manual approval.
+	Reason string `form:"reason" json:"reason" xml:"reason"`
+	// The desired username for the account.
+	// swagger:parameters
+	// pattern: [a-z0-9_]{2,64}
+	// example: a_valid_username
+	// required: true
+	Username string `form:"username" json:"username" xml:"username" binding:"required"`
+	// The email address to be used for login.
+	// swagger:parameters
+	// example: someone@wherever.com
+	// required: true
+	Email string `form:"email" json:"email" xml:"email" binding:"required"`
+	// The password to be used for login. This will be hashed before storage.
+	// swagger:parameters
+	// example: some_really_really_really_strong_password
+	// required: true
+	Password string `form:"password" json:"password" xml:"password" binding:"required"`
+	// The user agrees to the terms, conditions, and policies of the instance.
+	// swagger:parameters
+	// required: true
+	Agreement bool `form:"agreement"  json:"agreement" xml:"agreement" binding:"required"`
+	// The language of the confirmation email that will be sent.
+	// swagger:parameters
+	// example: en
+	// Required: true
+	Locale string `form:"locale" json:"locale" xml:"locale" binding:"required"`
+}
+
+type Follower struct {
+	Instance     string
+	AccessToken  string
+	UserId       string
+	ClientId     string
+	ClientSecret string
 }
 
 func main() {
 	var wg sync.WaitGroup
 
-	clientId := flag.String("client-id", "", "Activitypub client id")
-	clientSecret := flag.String("client-secret", "", "Activitypub client secret")
-	instance := flag.String("instance", "", "Activitypub instance")
-	accessToken := flag.String("access-token", "", "Activitypub access token")
-
-	clientIdSecond := flag.String("client-id-second", "", "Activitypub client id")
-	clientSecretSecond := flag.String("client-secret-second", "", "Activitypub client secret")
-	instanceSecond := flag.String("instance-second", "", "Activitypub instance")
-	accessTokenSecond := flag.String("access-token-second", "", "Activitypub access token")
-
-	userIdFirst := flag.String("userid-first", "", "First User Id")
-	userIdSecond := flag.String("userid-second", "", "Second User Id")
-	load := flag.String("load", "", "Numer of requests to run")
-	parallel := flag.String("parallel", "", "Parallel request or sequential")
+	instance := flag.String("instance", getEnv("SERVER_URL", ""), "Activitypub first instance")
+	instanceSecond := flag.String("instance-second", getEnv("SERVER_URL_SECOND", ""), "Activitypub second instance")
+	followersLocal := flag.Int("followers-local", 0, "No of local followers ( followers on same instance )")
+	followersFederated := flag.Int("followers-fed", 1, "No of federated followers ( followers on second instance )")
+	load := flag.Int("load", 1, "Numer of requests to run")
+	parallel := flag.Bool("parallel", false, "Parallel request or sequential")
+	deleteToots := flag.Bool("delete-toots", false, "Delete toots when test ends")
+	showOutputGraph := flag.Bool("show-graph", false, "Show output graph")
+	outputFileName := ""
 
 	flag.Parse()
 
-	if *clientId == "" || *clientSecret == "" || *instance == "" || *accessToken == "" {
+	if *instance == "" {
 		fmt.Println("All flags must be set, please --help for info")
 		os.Exit(3)
 	}
 
-	if *clientIdSecond != "" || *clientSecretSecond != "" || *instanceSecond != "" || *accessTokenSecond != "" {
-		if *clientIdSecond == "" || *clientSecretSecond == "" || *instanceSecond == "" || *accessTokenSecond == "" {
-			fmt.Println("All flags for second instance must be set, please --help for info")
-			os.Exit(3)
-		}
+	username := generateRandomString(10)
+	accessToken, userIdFirst, clientId, clientSecret, err := createUser(username, *instance)
+	if err != nil {
+		panic(fmt.Sprintf("Failed creating user: %+v\n", err))
 	}
 
 	client := mastodon.NewClient(&mastodon.Config{
 		Server:       *instance,
-		ClientID:     *clientId,
-		ClientSecret: *clientSecret,
-		AccessToken:  *accessToken,
+		ClientID:     clientId,
+		ClientSecret: clientSecret,
+		AccessToken:  accessToken,
 	})
 
 	timeline, err := client.GetTimelineHome(context.Background(), nil)
@@ -82,71 +110,65 @@ func main() {
 		fmt.Println(timeline[i])
 	}
 
-	if *userIdSecond != "" {
-		clientSecond := mastodon.NewClient(&mastodon.Config{
-			Server:       *instanceSecond,
-			ClientID:     *clientIdSecond,
-			ClientSecret: *clientSecretSecond,
-			AccessToken:  *accessTokenSecond,
-		})
-
-		accounts, err := clientSecond.Search(context.Background(), *userIdFirst, true)
-		if err != nil {
-			fmt.Printf("unable to get the account %s:\n", *userIdFirst)
-			panic(err)
+	if *instanceSecond != "" {
+		followers := []Follower{}
+		for i := 0; i < *followersFederated; i++ {
+			followers = append(followers,
+				Follower{Instance: *instanceSecond})
 		}
-
-		if len(accounts.Accounts) > 0 {
-			fmt.Printf("Successfully got account now tryiing to follow\n")
-			_, err := clientSecond.AccountFollow(context.Background(), accounts.Accounts[0].ID)
-			if err != nil {
-				fmt.Printf("unable to follow the account %s\n", *userIdFirst)
-				panic(err)
-			}
-		}
-
-		accounts, err = client.Search(context.Background(), *userIdSecond, true)
-		if err != nil {
-			fmt.Printf("unable to get the account %s:\n", *userIdSecond)
-			panic(err)
-		}
-
-		time.Sleep(1 * time.Second)
-
-		err = client.FollowRequestAuthorize(context.Background(), accounts.Accounts[0].ID)
-		if err != nil {
-			fmt.Printf("unable accept follow request %s:\n", *userIdSecond)
-			panic(err)
-		}
-
+		createAndAcceptFollowers(client, userIdFirst, followers)
 	}
 
-	// by default number of request should be 1.
-	numberRequests := 1
-	if *load != "" {
-		parseLoad, err := strconv.Atoi(*load)
-		if err != nil {
-			panic(fmt.Sprintf("Unable to parse load requests %s: %w", load, err))
+	if *followersLocal > 0 {
+		followers := []Follower{}
+		for i := 0; i < *followersLocal; i++ {
+			followers = append(followers,
+				Follower{Instance: *instance})
 		}
-		numberRequests = parseLoad
+		createAndAcceptFollowers(client, userIdFirst, followers)
 	}
-	parallelRequests := false
-	if *parallel != "" {
-		parallelRequests = true
-	}
+
+	numberRequests := *load
+	parallelRequests := *parallel
+	postResults := []time.Duration{}
+	deleteResults := []time.Duration{}
 
 	if parallelRequests {
 		for j := 0; j < numberRequests; j++ {
 			wg.Add(1)
 			go func() {
-				createToots(client, false)
+				postDuration, deleteDuration := createToots(client, *deleteToots)
+				postResults = append(postResults, postDuration)
+				deleteResults = append(deleteResults, deleteDuration)
 				defer wg.Done()
 			}()
 		}
 		wg.Wait()
+		if *instanceSecond == "" {
+			points := durationToPlotters(postResults)
+			plotGraph("parallel", points)
+			outputFileName = "parallel.png"
+		} else {
+			points := durationToPlotters(readMetrics(METRICS_PATH))
+			plotGraph("parallel-federated", points)
+			outputFileName = "parallel-federated.png"
+			
+		}
 	} else {
 		for j := 0; j < numberRequests; j++ {
-			createToots(client, false)
+			postDuration, deleteDuration := createToots(client, *deleteToots)
+			postResults = append(postResults, postDuration)
+			deleteResults = append(deleteResults, deleteDuration)
+
+		}
+		if *instanceSecond == "" {
+			points := durationToPlotters(postResults)
+			plotGraph("sequential", points)
+			outputFileName = "sequential.png"
+		} else {
+			points := durationToPlotters(readMetrics(METRICS_PATH))
+			plotGraph("sequential-federated", points)
+			outputFileName = "sequential-federated.png"
 		}
 	}
 
@@ -155,12 +177,314 @@ func main() {
 		fmt.Println("Error while fetching the timeline:\n")
 		panic(err)
 	}
-	fmt.Printf("Number of tweets in first user's public timeline %+v\n", len(timeline))
+	fmt.Printf("Number of tweets in first user's public timeline %+v\n\n", len(timeline))
 	timeline, err = client.GetTimelineHome(context.Background(), nil)
 	if err != nil {
 		fmt.Println("Error while fetching the timeline:\n")
 		panic(err)
 	}
-	fmt.Printf("Number of tweets in first user's home timeline %+v\n", len(timeline))
+	fmt.Printf("Number of tweets in first user's home timeline %+v\n\n", len(timeline))
+	fmt.Printf("Post durations %+v\n", postResults)
+	fmt.Printf("Metrics graph generated at %s\n", outputFileName)
 
+	if *showOutputGraph {
+		//runCommand("xdg-open", outputFileName)
+	}
+}
+
+func readMetrics(path string) []time.Duration {
+	result := []time.Duration{}
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	// optionally, resize scanner's capacity for lines over 64K, see next example
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+		line := scanner.Text()
+		split := strings.Split(line, " ")
+		durationStr := split[len(split)-1]
+		parsedDuration, err  := time.ParseDuration(durationStr)
+		if err != nil {
+			panic(fmt.Sprintf("Unable to parse duration from metrics %+v\n", err))
+		}
+		result = append(result, parsedDuration)
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(fmt.Sprintf("Unable to parse metrics %+v\n", err))
+	}
+	return result
+}
+
+func createAndAcceptFollowers(parentUserClient *mastodon.Client, parentUserId string, followers []Follower) {
+	for _, follower := range followers {
+
+		username := generateRandomString(10)
+		accessTokenSecond, userIdSecond, clientIdSecond, clientSecretSecond, err := createUser(username, follower.Instance)
+		if err != nil {
+			panic(fmt.Sprintf("Failed creating user: %+v\n", err))
+		}
+		follower.UserId = userIdSecond
+		follower.AccessToken = accessTokenSecond
+		follower.ClientId = clientIdSecond
+		follower.ClientSecret = clientSecretSecond
+
+		clientSecond := mastodon.NewClient(&mastodon.Config{
+			Server:       follower.Instance,
+			ClientID:     follower.ClientId,
+			ClientSecret: follower.ClientSecret,
+			AccessToken:  follower.AccessToken,
+		})
+
+		accounts, err := clientSecond.Search(context.Background(), parentUserId, true)
+		if err != nil {
+			fmt.Printf("unable to get the account %s:\n", parentUserId)
+			panic(err)
+		}
+
+		if len(accounts.Accounts) > 0 {
+			fmt.Printf("Successfully got account now tryiing to follow\n")
+			_, err := clientSecond.AccountFollow(context.Background(), accounts.Accounts[0].ID)
+			if err != nil {
+				fmt.Printf("unable to follow the account %s\n", parentUserId)
+				panic(err)
+			}
+		}
+		// If test fails with 404 while following a user, increase this timeout
+		time.Sleep(50 * time.Millisecond)
+
+		accounts, err = parentUserClient.Search(context.Background(), follower.UserId, true)
+		if err != nil {
+			fmt.Printf("unable to get the account %s:\n", follower.UserId)
+			panic(err)
+		}
+
+		err = parentUserClient.FollowRequestAuthorize(context.Background(), accounts.Accounts[0].ID)
+		if err != nil {
+			fmt.Printf("unable accept follow request %s and total accounts %+v:\n", follower.UserId, accounts.Accounts[0])
+			panic(err)
+		}
+	}
+}
+
+func createToots(client *mastodon.Client, deleteToot bool) (time.Duration, time.Duration) {
+	start := time.Now()
+	status, err := client.PostStatus(context.Background(), &mastodon.Toot{
+		Status: "Setting sample status",
+	})
+	if err != nil {
+		fmt.Printf("Error %w while setting the status/toot\n", err)
+		panic(err)
+	}
+	t := time.Now()
+	postDuration := t.Sub(start)
+	fmt.Printf("Duration: %+v to post a status\n", postDuration)
+
+	fmt.Println("Posted status id is: " + status.ID)
+
+	var deleteDuration time.Duration
+	if deleteToot {
+		start = time.Now()
+		// delete the newly created status
+		client.DeleteStatus(context.Background(), status.ID)
+		t = time.Now()
+		deleteDuration = t.Sub(start)
+		fmt.Printf("Duration: %+v to delete a status \n", postDuration, deleteDuration)
+	}
+	return postDuration, deleteDuration
+}
+
+func createUser(username, serverURL string) (string, string, string, string, error) {
+	domain := strings.Split(strings.Split(serverURL, "://")[1], "/")[0]
+	redirectURI := serverURL
+	clientName := "Test Application Name"
+	registrationReason := "Testing whether or not this dang diggity thing works!"
+	registrationUsername := username
+	registrationEmail := fmt.Sprintf("%s@%s", username, domain)
+	registrationPassword := "very good password 123"
+	registrationAgreement := true
+	registrationLocale := "en"
+
+	// Step 1: create the app to register the new account
+	createAppURL := fmt.Sprintf("%s/api/v1/apps", serverURL)
+	createAppData := map[string]string{
+		"client_name":   clientName,
+		"redirect_uris": redirectURI,
+	}
+	createAppResponse := makePostRequest(createAppURL, createAppData)
+	clientID := createAppResponse["client_id"].(string)
+	clientSecret := createAppResponse["client_secret"].(string)
+	fmt.Printf("Obtained client_id: %s and client_secret: %s\n", clientID, clientSecret)
+
+	// Step 2: obtain a code for that app
+	appCodeURL := fmt.Sprintf("%s/oauth/token", serverURL)
+	appCodeData := map[string]string{
+		"scope":         "read",
+		"grant_type":    "client_credentials",
+		"client_id":     clientID,
+		"client_secret": clientSecret,
+		"redirect_uri":  redirectURI,
+	}
+	appCodeResponse := makePostRequest(appCodeURL, appCodeData)
+	appAccessToken := appCodeResponse["access_token"].(string)
+	fmt.Printf("Obtained app access token: %s\n", appAccessToken)
+	accountRegisterData := AccountCreateRequest{
+		Reason:    registrationReason,
+		Email:     registrationEmail,
+		Username:  registrationUsername,
+		Password:  registrationPassword,
+		Agreement: registrationAgreement,
+		Locale:    registrationLocale,
+	}
+	accountRegisterHeaders := map[string]string{
+		"Content-Type":  "application/json; charset=UTF-8",
+		"Authorization": fmt.Sprintf("Bearer %s", appAccessToken),
+	}
+	// Step 3: use the code to register a new account
+	accountRegisterURL := fmt.Sprintf("%s/api/v1/accounts", serverURL)
+	accountRegisterDataJson, err := json.Marshal(accountRegisterData)
+	if err != nil {
+		return "", "", "", "", err
+	}
+	accountRegisterResponse := makePostRequestWithHeaders(accountRegisterURL, accountRegisterDataJson, accountRegisterHeaders)
+	fmt.Printf("Obtained user access token: %+v\n", accountRegisterResponse)
+	userAccessToken := accountRegisterResponse["access_token"].(string)
+	fmt.Printf("Obtained user access token: %s\n", userAccessToken)
+
+	// Step 4: verify the returned access token
+	verifyCredentialsURL := fmt.Sprintf("%s/api/v1/accounts/verify_credentials", serverURL)
+	verifyResponse := makeGetRequestWithHeaders(verifyCredentialsURL, map[string]string{"Authorization": fmt.Sprintf("Bearer %s", userAccessToken)})
+	fmt.Println(string(verifyResponse))
+
+	return userAccessToken, registrationEmail, clientID, clientSecret, nil
+}
+
+func runCommand(command string, args ...string) {
+	cmd := exec.Command(command, args...)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		log.Fatalf("Error running command: %s", err)
+	}
+	fmt.Println(out.String())
+}
+
+func getEnv(key, defaultValue string) string {
+	value, ok := os.LookupEnv(key)
+	if !ok {
+		value = defaultValue
+	}
+	return value
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyz0123456789")
+
+func generateRandomString(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
+
+func makePostRequest(url string, data map[string]string) map[string]interface{} {
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		log.Fatalf("Error encoding JSON: %s", err)
+	}
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		log.Fatalf("Error making POST request: %s", err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Error reading response body: %s", err)
+	}
+	var result map[string]interface{}
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		log.Fatalf("Error decoding JSON response: %s", err)
+	}
+	return result
+}
+
+func makePostRequestWithHeaders(url string, data []byte, headers map[string]string) map[string]interface{} {
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(data))
+	if err != nil {
+		log.Fatalf("Error creating POST request: %s", err)
+	}
+	for key, value := range headers {
+		req.Header.Set(key, value)
+	}
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalf("Error making POST request with headers: %s", err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Error reading response body: %s", err)
+	}
+	var result map[string]interface{}
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		log.Fatalf("Error decoding JSON response: %s", err)
+	}
+	return result
+}
+
+func makeGetRequestWithHeaders(url string, headers map[string]string) []byte {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatalf("Error creating GET request: %s", err)
+	}
+	for key, value := range headers {
+		req.Header.Set(key, value)
+	}
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalf("Error making GET request with headers: %s", err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Error reading response body: %s", err)
+	}
+	return body
+}
+
+func plotGraph(name string, durations plotter.XYs) {
+	p := plot.New()
+
+	p.Title.Text = name
+	p.X.Label.Text = "Requests"
+	p.Y.Label.Text = "Duration in ms"
+
+	err := plotutil.AddLinePoints(p,
+		"Requests", durations)
+	if err != nil {
+		panic(err)
+	}
+
+	// Save the plot to a PNG file.
+	if err := p.Save(5*vg.Inch, 5*vg.Inch, name+".png"); err != nil {
+		panic(err)
+	}
+}
+
+func durationToPlotters(n []time.Duration) plotter.XYs {
+	pts := make(plotter.XYs, len(n))
+	for i, v := range n {
+		pts[i].X = float64(i)
+		pts[i].Y = float64(v) / float64(time.Millisecond)
+	}
+	return pts
 }
--- run_test_federated.sh
+++ run_test_federated.sh
@@ -1,6 +1,3 @@
-#!/bin/sh
-# Parts of this script is copied from https://github.com/superseriousbusiness/gotosocial/blob/main/scripts/auth_flow.sh
-
 set -eux
 
 sudo rm -f /tmp/tweet_fan_out_metrics
@@ -9,63 +6,7 @@ sudo rm -f /tmp/tweet_fan_out_metrics
 make clean || true
 make build
 
-SERVER_URL="${SERVER_URL}"
-SERVER_URL_SECOND="${SERVER_URL_SECOND}"
-
-# parse domain name from SERVER_URL
-DOMAIN=$(echo ${SERVER_URL} | awk -F[/:] '{print $4}')
-DOMAIN_SECOND=$(echo ${SERVER_URL_SECOND} | awk -F[/:] '{print $4}')
-
-# generate fake usernames
-USERNAME=$(tr -dc a-z0-9 </dev/urandom | head -c 10; echo)
-
-REDIRECT_URI="${SERVER_URL}"
-REDIRECT_URI_SECOND="${SERVER_URL_SECOND}"
-CLIENT_NAME="Test Application Name"
-REGISTRATION_REASON="Testing whether or not this dang diggity thing works!"
-REGISTRATION_USERNAME="${USERNAME}"
-REGISTRATION_EMAIL="${USERNAME}@${DOMAIN}"
-REGISTRATION_PASSWORD="very good password 123"
-REGISTRATION_AGREEMENT="true"
-REGISTRATION_LOCALE="en"
-REGISTRATION_USERNAME_SECOND="${USERNAME}"
-REGISTRATION_EMAIL_SECOND="${USERNAME}@${DOMAIN_SECOND}"
-
-# Step 1: create the app to register the new account
-CREATE_APP_RESPONSE=$(curl --fail -s -X POST -F "client_name=${CLIENT_NAME}" -F "redirect_uris=${REDIRECT_URI}" "${SERVER_URL}/api/v1/apps")
-CLIENT_ID=$(echo "${CREATE_APP_RESPONSE}" | jq -r .client_id)
-CLIENT_SECRET=$(echo "${CREATE_APP_RESPONSE}" | jq -r .client_secret)
-echo "Obtained client_id: ${CLIENT_ID} and client_secret: ${CLIENT_SECRET}"
-# Step 2: obtain a code for that app
-APP_CODE_RESPONSE=$(curl --fail -s -X POST -F "scope=read" -F "grant_type=client_credentials" -F "client_id=${CLIENT_ID}" -F "client_secret=${CLIENT_SECRET}" -F "redirect_uri=${REDIRECT_URI}" "${SERVER_URL}/oauth/token")
-APP_ACCESS_TOKEN=$(echo "${APP_CODE_RESPONSE}" | jq -r .access_token)
-echo "Obtained app access token: ${APP_ACCESS_TOKEN}"
-# Step 3: use the code to register a new account
-ACCOUNT_REGISTER_RESPONSE=$(curl --fail -s -H "Authorization: Bearer ${APP_ACCESS_TOKEN}" -F "reason=${REGISTRATION_REASON}" -F "email=${REGISTRATION_EMAIL}" -F "username=${REGISTRATION_USERNAME}" -F "password=${REGISTRATION_PASSWORD}" -F "agreement=${REGISTRATION_AGREEMENT}" -F "locale=${REGISTRATION_LOCALE}" "${SERVER_URL}/api/v1/accounts")
-USER_ACCESS_TOKEN=$(echo "${ACCOUNT_REGISTER_RESPONSE}" | jq -r .access_token)
-echo "Obtained user access token: ${USER_ACCESS_TOKEN}"
-# # Step 4: verify the returned access token
-curl -s -H "Authorization: Bearer ${USER_ACCESS_TOKEN}" "${SERVER_URL}/api/v1/accounts/verify_credentials" | jq
-
-# Note: Refactor code block below is a copy of code block above, refactor and make it one function.
-# Step 1: create the app to register the new account
-CREATE_APP_RESPONSE_SECOND=$(curl --fail -s -X POST -F "client_name=${CLIENT_NAME}" -F "redirect_uris=${REDIRECT_URI_SECOND}" "${SERVER_URL_SECOND}/api/v1/apps")
-CLIENT_ID_SECOND=$(echo "${CREATE_APP_RESPONSE_SECOND}" | jq -r .client_id)
-CLIENT_SECRET_SECOND=$(echo "${CREATE_APP_RESPONSE_SECOND}" | jq -r .client_secret)
-echo "Obtained client_id: ${CLIENT_ID_SECOND} and client_secret: ${CLIENT_SECRET_SECOND}"
-# Step 2: obtain a code for that app
-APP_CODE_RESPONSE_SECOND=$(curl --fail -s -X POST -F "scope=read" -F "grant_type=client_credentials" -F "client_id=${CLIENT_ID_SECOND}" -F "client_secret=${CLIENT_SECRET_SECOND}" -F "redirect_uri=${REDIRECT_URI_SECOND}" "${SERVER_URL_SECOND}/oauth/token")
-APP_ACCESS_TOKEN_SECOND=$(echo "${APP_CODE_RESPONSE_SECOND}" | jq -r .access_token)
-echo "Obtained app access token: ${APP_ACCESS_TOKEN_SECOND}"
-# Step 3: use the code to register a new account
-ACCOUNT_REGISTER_RESPONSE_SECOND=$(curl --fail -s -H "Authorization: Bearer ${APP_ACCESS_TOKEN_SECOND}" -F "reason=${REGISTRATION_REASON}" -F "email=${REGISTRATION_EMAIL_SECOND}" -F "username=${REGISTRATION_USERNAME_SECOND}" -F "password=${REGISTRATION_PASSWORD}" -F "agreement=${REGISTRATION_AGREEMENT}" -F "locale=${REGISTRATION_LOCALE}" "${SERVER_URL_SECOND}/api/v1/accounts")
-USER_ACCESS_TOKEN_SECOND=$(echo "${ACCOUNT_REGISTER_RESPONSE_SECOND}" | jq -r .access_token)
-echo "Obtained user access token: ${USER_ACCESS_TOKEN_SECOND}"
-# # Step 4: verify the returned access token
-curl -s -H "Authorization: Bearer ${USER_ACCESS_TOKEN_SECOND}" "${SERVER_URL_SECOND}/api/v1/accounts/verify_credentials" | jq
-
-
 # Run test for this user
-./main -client-id ${CLIENT_ID} -client-secret ${CLIENT_SECRET} -access-token ${USER_ACCESS_TOKEN} -instance ${SERVER_URL} -userid-first ${REGISTRATION_EMAIL} -client-id-second ${CLIENT_ID_SECOND} -client-secret-second ${CLIENT_SECRET_SECOND} -access-token-second ${USER_ACCESS_TOKEN_SECOND} -instance-second ${SERVER_URL_SECOND} -userid-second ${REGISTRATION_EMAIL_SECOND} -load 100 -parallel true
+./main -instance https://one.localhost -instance-second https://two.localhost -load 100 -followers-fed 50 -parallel -show-graph
 cat /tmp/tweet_fan_out_metrics
-cat /tmp/tweet_fan_out_metrics | awk '{print $15}' | sed 's/\ms$//' | gnuplot -p -e 'set xtics 1; set ylabel "duration (ms)"; set xlabel "Parallel request no"; plot "/dev/stdin" using 1 title "Duration" w linespoints pt 7'
+#cat /tmp/tweet_fan_out_metrics | awk '{print $15}' | sed 's/\ms$//' | gnuplot -p -e 'set xtics 1; set ylabel "duration (ms)"; set xlabel "Parallel request no"; plot "/dev/stdin" using 1 title "Duration" w linespoints pt 7'
